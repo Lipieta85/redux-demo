d06dd318a144308fe3c48ef7e72f2c7c7fc59026
Rafał Lipieta <cwgflojdid@wp.pl>
2018-08-11 08:21:32 +0200

    Update styles

--- src/actions.js
+++ src/actions.js
@@ -6,7 +6,6 @@ export const EDIT_COMMENT = 'EDIT_COMMENT';
 export const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT';
 export const THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
 
-// Add comment
 
 export function addComment(text) {
   return {
@@ -16,16 +15,12 @@ export function addComment(text) {
   }
 };
 
-// Remove comment
-
 export function removeComment(id) {
   return {
     type: REMOVE_COMMENT,
     id 
   }
 }
-
-// Edit comment
 
 export function editComment(text, id) {
   return {
@@ -35,8 +30,6 @@ export function editComment(text, id) {
   }
 }
 
-// THUMB_UP_COMMENT
-
 export function thumbUpComment(commentId) {
   return {
     type: THUMB_UP_COMMENT,
@@ -44,8 +37,6 @@ export function thumbUpComment(commentId) {
   }
 }
 
-// THUMB_DOWN_COMMENT
-  
 export function thumbDownComment (commentId) {
   return {
     type: THUMB_DOWN_COMMENT,
--- src/index.js
+++ src/index.js
@@ -20,5 +20,5 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-store.dispatch(addComment('pierwszy komentarz'));
-store.dispatch(addComment('drugi komentarz'));
+store.dispatch(addComment('Pierwszy komentarz'));
+store.dispatch(addComment('Drugi komentarz'));
